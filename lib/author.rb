95972d0d1d11d16160c104217e4dacb15a9b80b3
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-03-14 18:02:15 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,31 +1,4 @@
 class Author 
   
-  @@total_songs = 0
-  attr_accessor :songs, :name  
-  
-  def initialize(name)
-    @name = name
-    @songs = []
-  end 
-  
-  def add_song(song)
-    @songs << song
-    song.artist = self
-    @@total_songs += 1
-  end 
-  
-  def songs
-    @songs 
-  end 
-  
-  def add_song_by_name(song_name)
-    new_song = Song.new(song_name)
-    @songs << new_song
-    new_song.artist = self
-    @@total_songs += 1
-  end
-  
-  def self.song_count
-    @@total_songs
-  end 
+
 end 
