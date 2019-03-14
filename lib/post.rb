535505020f4b272bfb83724dfd2ff47ff0fa0829
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-03-14 18:05:45 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,23 +1,5 @@
-class Song
-  attr_accessor :artist
-  attr_reader :name 
-  @@all = []
+class Post
   
-  def initialize(name)
-    @name = name
-    @@all << self
-  end 
   
-  def self.all
-    @@all
-  end
-  
-  def artist_name
-    if self.artist.class == Artist
-      self.artist.name
-    else
-      nil
-    end
-  end 
   
 end 
